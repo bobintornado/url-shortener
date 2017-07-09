7b90e81041cefb43a8ac27463f3db12c9c2a0f3d
Bob Cao <bobintornado@gmail.com>
2017-07-09 15:15:37 +0800

    Record approximate unique visitor by cookie

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,6 @@
 class AppController < ApplicationController
+  include ::ActionController::Cookies
+
   RECORDED_ATTRIBUTES = %w[HTTP_VERSION HTTP_USER_AGENT HTTP_ACCEPT_LANGUAGE REMOTE_ADDR SERVER_NAME].freeze
 
   def redirect
@@ -6,6 +8,7 @@ class AppController < ApplicationController
     @url = Url.find(id)
 
     record_visit
+    record_unique_visitor
     redirect_to @url.url
   end
 
@@ -14,5 +17,15 @@ class AppController < ApplicationController
     args = http_info.merge(url: @url)
 
     Visit.create(args)
+  end
+
+  def record_unique_visitor
+    visitor_uuid = cookies.signed[:visitor_uuid]
+    if visitor_uuid
+      UniqueVisit.where(visitor_uuid: visitor_uuid, url: @url).first_or_create
+    else # new `visitor`
+      unique_visit = UniqueVisit.create(url: @url).reload
+      cookies.permanent.signed[:visitor_uuid] = unique_visit.visitor_uuid
+    end
   end
 end
