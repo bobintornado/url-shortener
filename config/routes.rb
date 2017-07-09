4e344ab6c4e7ac0b9b0276f848e9ddd883679a54
Bob Cao <bobintornado@gmail.com>
2017-07-09 21:47:47 +0800

    Add stats query endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :urls, only: [:index, :show, :create]
+    resources :urls, only: [:index, :show, :create] do
+      collection do
+        get 'unique_visitors_stat'
+      end
+    end
   end
   
   get '/:short_url', to: 'app#redirect' # record request info and redirect
